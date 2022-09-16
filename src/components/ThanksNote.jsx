b5fe6710f6f4df8fee00e0f43d976c441402a102
Ganesh sai <ganeshsai1346@gmail.com>
2022-09-16 12:08:26 +0200

    changes made

--- src/components/ThanksNote.jsx
+++ src/components/ThanksNote.jsx
@@ -8,7 +8,7 @@ const ThanksNote = () => {
       <div className="wrapper-1">
         <div className="wrapper-2">
           <h1>Thank you !</h1>
-          <p>Thanks for subscribing </p>
+          <p>Thanks for subscribing Music World </p>
           <p>You will receive a confirmation email soon </p>
           <Link to="/">
             <button className="go-home">go home</button>
